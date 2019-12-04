5478bce10753d0c320965d5661787a76984a6faf
Lucas <lucas.beretti@gmail.com>
2019-12-04 15:16:20 +0100

    Monaco push for Julien
    begin of the Monaco page

---
+++ src/components/sandbox/Sandbox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class = "container h-100">
+        <aside class = "container h-100 card border-solid"> 
+            <div class = "container border-solid justify-content-center align-self-center">
+                Ceci est l'énoncé de l'exercice
+            </div>     
+            <div class= "container border-solid justify-content-center">
+                Ceci sont les données d'entrée
+            </div>
+        </aside>
+
+        <div id = "container">
+            <script src="../../../node_modules/monaco-editor/min/vs/loader.js"> </script>        
+        </div>
+    </div>
+</template>
