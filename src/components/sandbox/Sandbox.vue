37040c6306a3b943bbb5d4822015ad1ee8d7ae0e
Julien <benardjr@gmail.com>
2019-12-05 15:50:10 +0100

    Monaco almost works with Vue
    but not quite yet

--- src/components/sandbox/Sandbox.vue
+++ src/components/sandbox/Sandbox.vue
@@ -9,8 +9,56 @@
             </div>
         </aside>
 
-        <div id = "container">
-            <script src="../../../node_modules/monaco-editor/min/vs/loader.js"> </script>        
-        </div>
+        <div id = "container"></div>
+    <button id = "runButton" type = "button"> Run </button>
+    <button id = "submitButton" type = "button"> Submit </button>
     </div>
 </template>
+
+<script src="../../../node_modules/monaco-editor/min/vs/loader.js">
+    export default {
+        data() {
+            return {
+                exercise : { id: '',
+                    name: '',
+                    description: '',
+                    exampleCode: '',
+                    testData: []
+                    },
+                code:''
+            }
+        },
+        created() {
+        require.config({ paths: { 'vs': '../../../node_modules/monaco-editor/min/vs' }});
+        require(['vs/editor/editor.main'], function() {
+            var editor = monaco.editor.create(document.getElementById('container'), {
+                value: "",
+                language: 'javascript',
+                theme: ''
+            });
+            editor.getModel().onDidChangeContent((event)=>{this.code=editor.getValue()});
+        });
+           //initialize the exercise
+        },
+        methods: {
+            submit() {
+               
+            },
+            run(){
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        height: 100px;
+        margin-top: 8%;
+    }
+    #error-message{
+        color: #ff6666;
+        font-size: 14px;
+    }
+</style>
