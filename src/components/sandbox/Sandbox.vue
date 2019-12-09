31b62fcc17d014e605652ba0a94e3f0902e9d127
Julien <benardjr@gmail.com>
2019-12-09 22:56:42 +0100

    Monaco works but it has nooo style

--- src/components/sandbox/Sandbox.vue
+++ src/components/sandbox/Sandbox.vue
@@ -1,16 +1,32 @@
 <template>
-  <div id="app">
-    <MonacoEditor
+<div class = "h-100">
+        <aside class = "container h-100 card border-solid"> 
+            <div class = "container border-solid justify-content-center align-self-center">
+                Description:<br>
+                {{exercise.description}}
+            </div>     
+            <div class= "container border-solid justify-content-center">
+                Données d'entrée:<br>
+                {{exercise.testData}}
+            </div>
+        </aside>
+        <div>
+        <b-form-select v-model="selected" :options="langs" @input="updatelang"></b-form-select>
+        <MonacoEditor
                 height="300"
                 width="1200"
-                theme='vsc'
-                :language="initlang"
-                :code="initcode"
+                :language="selected"
+                :code="code"
                 :editorOptions="options"
                 @mounted="onMounted"
                 @codeChange="onCodeChange"
                 >
         </MonacoEditor>
+        </div>
+    <div class="bobblockbuttons">
+    <b-button id = "runButton" type = "button" @click="run"> Run </b-button>
+    <b-button id = "submitButton" type = "button" @click="submit"> Submit </b-button>    
+    </div>
   </div>
 </template>
 
@@ -21,24 +37,60 @@ import {editor} from 'monaco-editor';
 export default {
   data () {
     return {
-      initcode: '//type here',
-      initlang:"javascript",
-      options: {
+        exercise : { id: '00000',
+                    name: 'test',
+                    description: 'Une description',
+                    exampleCode: '',
+                    testData: []
+                    },
+        code: '//type here',
+        options: {
         selectOnLineNumbers: true,
-      }
+        snippetSuggestions: true,
+        theme: "vs-dark"
+      },
+       selected: "javascript",
+        langs: [
+          { value: "javascript", text: 'JavaScript' },
+          { value: "python", text: 'Pyhton'}],
+        monedit:null,
     }
   },
+  created(){
+      alert('here we should get the exercise data from the api')
+  },
   methods:{
-  onCodeChange(monedit) {
-      alert(monedit.getValue());
+  onCodeChange(edit) {
+      this.code=edit.getValue();
   },
-  onMounted(monedit) {
-      editor.setModelLanguage(monedit.getModel(), "python");
-      alert(monedit.getModel().getModeId());
+  onMounted(edit) {
+      this.monedit=edit;
   },
+  updatelang(){
+      editor.setModelLanguage(this.monedit.getModel(), this.selected);
+  },
+    submit() {
+        alert('submit for exercise'+this.exercise.id+'\n with code:'+this.code+'\n with example code:'+this.exercise.exampleCode+'\n and testdata : '+this.exercise.testData)
+               
+            },
+    run(){
+        alert('run for exercise'+this.exercise.id+'\n with code:'+this.code+'\n with example code:'+this.exercise.exampleCode+'\n and testdata : '+this.exercise.testData)
+
+            }
   },
   components: {
     MonacoEditor
   },
 }
 </script>
+
+<style>
+    .container {
+        height: 100px;
+        margin-top: 8%;
+    }
+
+    .bobblockbuttons {
+        height: 38px;
+    }
+</style>
